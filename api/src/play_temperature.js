c3061b3181144548bc0574a4aef0aa846053baee
Kuba <jakub.mazurek96@gmail.com>
2017-08-12 22:27:25 +0200

    api | play_temperature test

--- api/src/play_temperature.js
+++ api/src/play_temperature.js
@@ -2,6 +2,9 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path')
 const sensor = require('ds18x20')
+
+const {SoundPlayer} = require('./lib/sound_player.js')
+
 function getTemp() {
 	return new Promise((resolve,reject) => {
 		sensor.get('28-0000092a6299', function(err, temp) {
@@ -15,14 +18,14 @@ function monitorTemp() {
 	if (fs.existsSync(filepath)) {
 		temperature = JSON.parse(fs.readFileSync(filepath));
 	}
-	console.log(temperature);
+	//console.log(temperature);
 	getTemp().then((temp) => {
 		console.log(temp)
 		temperature[Date.now()] = temp;
 		const tempString = JSON.stringify(temperature);
-		console.log(filepath);
+		//console.log(filepath);
 		const writeRes = fs.writeFileSync(filepath, tempString)
-		console.log(tempString, writeRes)
+		//console.log(tempString, writeRes)
 	})
 	.catch((e) => {console.log(e)})
 }
@@ -30,34 +33,40 @@ function play(degree) {
 	degree = ~~degree
 	console.log(degree);
 	const basicPath = path.join(__dirname, './src/res/')
-	let command = `play ${basicPath}hello.mp3 && play ${basicPath}haha.mp3 && play ${basicPath}is.mp3 && `
+	const sounds = [];
+
+	sounds.push(`${basicPath}hello.mp3`)  
+	sounds.push(`${basicPath}haha.mp3`)
+	sounds.push(`${basicPath}is.mp3 `)
+
 	if (degree < 18) {
-		command += `play ${basicPath}less.mp3 && `
+		sounds.push(`${basicPath}less.mp3`)
 	}
 	else if (degree <= 20) {
-		command += `play ${basicPath}${degree}.mp3 && `
+		sounds.push(`${basicPath}${degree}.mp3`)
 	}
 	else if (degree <=29) {
-		command += `play ${basicPath}${(~~(degree / 10)) * 10}.mp3 && `
-		command += `play ${basicPath}${(degree % 10)}.mp3 && `
+		sounds.push(`${basicPath}${(~~(degree / 10)) * 10}.mp3`)
+		sounds.push(`${basicPath}${(degree % 10)}.mp3`)
 	}
 	else {
-		command += `play ${basicPath}more.mp3 && `
-		command += `play ${basicPath}${20}.mp3 && `
-		command += `play ${basicPath}${9}.mp3 && `	
+		sounds.push(`${basicPath}more.mp3`)
+		sounds.push(`${basicPath}${20}.mp3`)
+		sounds.push(`${basicPath}${9}.mp3`)	
 	}
-	command += `play ${basicPath}degree.mp3`
-	//console.log(command);
-	exec(command, (err, stdout, stderr) => {
-	  if (err) {
-	    console.log('err', err);
-	    return;
-	  }
+	sounds.push(`${basicPath}degree.mp3`)
+	console.log(sounds);
+	SoundPlayer.play(sounds)
+	// exec(command, (err, stdout, stderr) => {
+	//   if (err) {
+	//     console.log('err', err);
+	//     return;
+	//   }
 
-	  // the *entire* stdout and stderr (buffered)
-	  console.log(`stdout: ${stdout}`);
-	  console.log(`stderr: ${stderr}`);
-	});
+	//   // the *entire* stdout and stderr (buffered)
+	//   console.log(`stdout: ${stdout}`);
+	//   console.log(`stderr: ${stderr}`);
+	// });
 }
 
 module.exports = {
